77916eb2ccaf66c29f657653adc930b8d74f6f88
nidhitvaishnav <nidhitvaishnav@gmail.com>
2018-07-14 11:12:29 -0500

    added tuples.py

---
+++ day3/tuples.py
@@ -0,0 +1,14 @@
+'''
+tuple: parentheses
+'''
+
+myTuple = ("abc", "mn", "xyz")
+
+print(myTuple[1])
+print(type(myTuple))
+
+'''
+Tuple is immutable. So changing the value of tuple will give error.
+'''
+# myTuple[1]="hello"
+# print(myTuple[1])
